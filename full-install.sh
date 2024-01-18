add7f5b86e37f2389199322b69a1a0837d41d767
Nemanja Vukmirovic <nemanja@lizzardsolutions.com>
2024-01-18 18:42:49 +0100

    fix: still exiting

--- full-install.sh
+++ full-install.sh
@@ -155,7 +155,8 @@ fi
 
 check_git_access() {
     # Attempt to SSH into the server with a timeout of 10 seconds
-    if ssh -o ConnectTimeout=10 -q $GIT_SERVER exit && echo "connected" ; then
+    if (ssh -o ConnectTimeout=10 -q $GIT_SERVER exit && echo "connected") ; then
+     
       cecho "BLUE" "SSH access to Git server verified."
     else
         cecho "RED" "Git server access failed. Updating SSH key in Airtable..."
