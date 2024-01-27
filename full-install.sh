a1279607e31a0e33f3267b5197c94b44a96cd1d9
Nemanja Vukmirovic <nemanja@lizzardsolutions.com>
2024-01-27 08:41:05 +0100

    fix: updated version #4

--- full-install.sh
+++ full-install.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+#Version: 0.4
+
 GIT_SERVER="ssh://git@fleet-flows-git.lizzardsolutions.com"
 AIRTABLE_BASE_ID="appYWVOaoPhQB0nmA"
 AIRTABLE_TABLE_NAME="Unipi" 
-HOSTNAME=$(hostname)
+HOSTNAME=$(hostname -f)
 SSH_KEY_PATH="$HOME/.ssh/id_rsa"
 BRANCH="main" 
 
@@ -10,7 +12,17 @@ BRANCH="main"
 debug=false
 noupdate=true
 AIRTABLE_API_KEY=""
-cecho(){
+
+# Constants
+BRANCH="main"
+SSH_KEY_PATH="$HOME/.ssh/id_rsa"
+
+AUTO_UPDATER_SCRIPT="/usr/local/bin/auto_updater_ffjs.sh"
+LOG_FILE="/var/log/auto_updater_ffjs.log"
+RESTART_SCRIPT="/usr/local/bin/restart_change_ffjs.sh"
+
+
+function cecho(){
     RED="\033[0;31m"
     GREEN="\033[0;32m"  # <-- [0 means not bold
     YELLOW="\033[1;33m" # <-- [1 means bold
@@ -25,46 +37,25 @@ cecho(){
     printf "${!1}${2} ${NC}\n" # <-- bash
 }
 
-while [ "$#" -gt 0 ]; do
-  case "$1" in
-    -d|--debug)
-      debug=true
-      shift
-      ;;
-    -nou|--no-update)
-      noupdate=false
-      shift
-      ;;
-    -k|--key)
-      AIRTABLE_API_KEY="$2"
-      shift 2
-      ;;
-    *)
-      echo "Error: Unsupported flag $1" >&2
-      exit 1
-      ;;
-  esac
-done
+function debug_echo() {
+  if [ "$debug" = true ]; then
+    cecho "$1" "$2"
+  fi
+}
 
-
-debug_echo() {
-    if [ "$debug" = true ]; then
-        cecho "$1" "$2"
+function check_git_access() {
+    # Attempt to SSH into the server with a timeout of 10 seconds
+		ssh -q -o ConnectTimeout=120 ${GIT_SERVER##*/} 'exit' 2>/dev/null
+		if [ $? -eq 0 ]; then
+    	echo "SSH access to Git server verified."
+    	return 0
+    else
+    	echo "RED" "Git server access failed"
+			return 1
     fi
 }
-debug_echo "DEBUG" "Debugging "
 
-
-if [ -z "$AIRTABLE_API_KEY" ]; then
-    cecho "RED" "Error: No Airtable API key provided."
-    cecho "CYAN" "Usage: $0 -k <AIRTABLE_API_KEY>"
-    exit 1
-fi
-
-
-
-
-check_airtable_api_key() {
+function check_airtable_api_key() {
     debug_echo "DEBUG" "check_airtable_api_key called"
     # Making a test request to Airtable
     local response=$(curl -s -o /dev/null -w "%{http_code}" -X GET \
@@ -73,86 +64,26 @@ check_airtable_api_key() {
 
     # Check if the HTTP status code is 200 (OK)
     if [ "$response" -ne 200 ]; then
-
-        cecho "RED" "Invalid API key. Stopping the script."
-        exit 1
+      cecho "RED" "Invalid API key. Stopping the script."
+      exit 1
     else
-        cecho "GREEN" "API key is valid."
-        # Continue with the rest of the script
+      cecho "GREEN" "API key is valid."
+      # Continue with the rest of the script
     fi
 }
 
-debug_echo "DEBUG" "noupdate flasg is: $noupdate"
-if [ "$noupdate" = true ]; then
-    debug_echo "DEBUG" "running update"
-    sudo apt update
-    debug_echo "DEBUG" "running upgrade"
-    sudo apt upgrade
-    fi
-
-debug_echo "DEBUG" "calling check_airtable_api_key"
-check_airtable_api_key 
-
 # Check and install required packages
-ensure_installed() {
+function ensure_installed() {
     
     if ! command -v $1 &> /dev/null; then
         cecho "YELLOW" "Installing $1..."
-        sudo apt install -y $1
+        sudo apt -y install $1
     else
         cecho "YELLOW" "$1 is already installed."
     fi
 }
-debug_echo "DEBUG" "updating npm to the latest version"
-# Update npm to the latest version
 
-# Check if 'n' is installed
-
-
-# Update Node.js to the latest version using 'n'
-debug_echo "DEBUG" " installing specific version of node"
-
-
-
-cecho "GREEN" "Node.js and npm are updated to the latest versions."
-
-# Install Node.js, npm, and Node-RED
-ensure_installed inotify-tools 
-ensure_installed git
-ensure_installed jq
-ensure_installed nano
-
-
-debug_echo "DEBUG" "checking if n is installed"
-if [ -f "/usr/local/bin/n" ]; then
-debug_echo "DEBUG" "installed /usr/local/bin/n"
-elif [ -f "/usr/bin/n" ]; then
-debug_echo "DEBUG" "installed /usr/bin/n"
-else
-    cecho "YELLOW" "n is not installed. Installing n..."
-    # Install n (Node.js version manager)
-    sudo npm install -g n
-fi
-
- sudo n install 18 
-
-debug_echo "DEBUG" "checking if node-red is installed"
-if [ -f "/usr/local/bin/node-red" ]; then
-debug_echo "DEBUG" "installed /usr/local/bin/node-red"
-elif [ -f "/usr/bin/node-red" ]; then
-debug_echo "DEBUG" "installed /usr/bin/node-red"
-else
-    bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered)  --confirm-install  --node18  
-fi
-
-
-# Check and generate SSH key
-if [ ! -f "$SSH_KEY_PATH" ]; then
-    cecho "GREEN" "Generating new SSH key..."
-    ssh-keygen -t rsa -b 4096 -f $SSH_KEY_PATH -N ""
-fi
-
-create_airtable_record() {
+function create_airtable_record() {
     local hostname=$1
     local ssh_key=$2
 
@@ -170,7 +101,8 @@ create_airtable_record() {
     #echo "Response from Airtable:"
     #echo "$response"
 }
-update_airtable_record() {
+
+function update_airtable_record() {
     debug_echo "DEBUG" "update_airtable_record called"
 
     local record_id=$1
@@ -188,8 +120,7 @@ update_airtable_record() {
     #echo "$response"
 }
 
-
-fetch_airtable_record_id_by_hostname() {
+function fetch_airtable_record_id_by_hostname() {
      debug_echo "DEBUG" "fetch_airtable_record_id_by_hostname called"
     local hostname=$1
     # Encoding the formula: {Device id} = 'hostname'
@@ -204,7 +135,7 @@ fetch_airtable_record_id_by_hostname() {
 }
 
 # Check Git access and update Airtable if necessary
-update_ssh_key_in_airtable() {
+function update_ssh_key_in_airtable() {
     debug_echo "DEBUG" "update_ssh_key_in_airtable called"
     local pubkey=$(cat $SSH_KEY_PATH.pub)
     local record_id=$(fetch_airtable_record_id_by_hostname "$HOSTNAME")
@@ -222,52 +153,30 @@ update_ssh_key_in_airtable() {
     fi
 }
 
-
-check_git_access() {
-    # Attempt to SSH into the server with a timeout of 10 seconds
-    if (ssh -o ConnectTimeout=10 -q $GIT_SERVER echo "connected" && exit); then
-     echo "SSH access to Git server verified."
-     return 0 
-    else
-        update_ssh_key_in_airtable
-        echo "RED" "Git server access failed"
-        
-    fi
-}
- 
-
- debug_echo "DEBUG" "checking git access"
- check_git_access
- debug_echo "DEBUG" "Git access successfull"
-
-# Constants
-BRANCH="main"
-SSH_KEY_PATH="$HOME/.ssh/id_rsa"
-cd $HOME
-# Function to clone Git repositories
- debug_echo "DEBUG" "cloning repository"
-clone_repository() {
+function clone_repository() {
     local repo_name=$1
     local branch=$2
-    until git clone --single-branch --branch ${branch} ${GIT_SERVER}/${repo_name}; do
-        cecho "YELLOW" "Git clone of $repo_name failed. Retrying..."
-        sleep 5
+		while [ 1 -eq 1 ]; do
+     	
+			git clone --single-branch --branch ${branch} ${GIT_SERVER}/${repo_name} 2>&1 | tee /tmp/git_output.txt
+			if [ $? -ne 0 ]; then
+				PERM_DENIED=$(sed -n '/Permissions denied/=' /tmp/git_output.txt)
+				if [ "${PERM_DENIED}" ]; then
+					cecho "YELLOW" "Git clone of $repo_name failed. Retrying..."
+		      sleep 60
+				else
+					cecho "RED" "Git clone of $repo_name error. Exiting..."
+					exit 1;	
+				fi
+			else	# git succeeded
+				break;	# stop the loop
+			fi
     done
 }
 
-# Clone repositories
-clone_repository "fleet-files" ${BRANCH}
-clone_repository "fleet-flows-js" ${BRANCH}
-
-# Run npm install in fleet-flows-js
-cd fleet-flows-js
-npm install
-cd ..
-
 # Create .env file
-create_env_file() {
-    local env_file_path="$1/.env"
-    cat <<EOL > $env_file_path
+function create_env_file() {
+    cat > .env <<EOL
 LOCAL_REPO_PATH=$HOME/fleet-files
 MASTER_BRANCH=main
 FLOW_FILE_PATH=$HOME/.node-red/flows.json
@@ -281,19 +190,17 @@ RESTART_COMMAND='find $HOME/fleet-files -maxdepth 1 -type f -exec cp {} $HOME/.n
 EOL
 }
 
-create_env_file "$(realpath ./fleet-flows-js)"
-
 # Create a systemd service for fleet-flows-js
-create_systemd_service() {
-    local service_path="/etc/systemd/system/fleet-flows-js.service"
-    sudo bash -c "cat > $service_path" <<EOL
+function create_systemd_service() {
+	
+    cat >/etc/systemd/system/fleet-flows-js.service <<EOL
 [Unit]
 Description=OTA flow updates and flow compiling
 After=network.target
 
 [Service]
 ExecStart=/usr/bin/npm run start--force
-WorkingDirectory=$(realpath ./fleet-flows-js)
+WorkingDirectory=$HOME/fleet-flows-js)
 Restart=always
 User=$(whoami)
 
@@ -306,11 +213,8 @@ EOL
     sudo systemctl start fleet-flows-js.service
 }
 
-create_systemd_service
-
 # Create auto update job
-create_auto_update_job() {
-    AUTO_UPDATER_SCRIPT="/usr/local/bin/auto_updater_ffjs.sh"
+function create_auto_update_job() {
 
     sudo tee $AUTO_UPDATER_SCRIPT > /dev/null <<EOL
 #!/bin/bash
@@ -323,7 +227,7 @@ FILES_TO_BACKUP=("schema.yml" ".env") # Add other files as needed
 BRANCH=${BRANCH}
 if [ -d "\$PROJECT_DIR" ] && [ -d "\$PROJECT_DIR/.git" ]; then
     cd \$PROJECT_DIR
-    echo "Current Directory: $(pwd)"
+    echo "Current Directory: \$(pwd)"
 
     # Fetch the latest commits from the remote
     git fetch
@@ -376,23 +280,22 @@ else
 fi
 EOL
     sudo chmod +rx $AUTO_UPDATER_SCRIPT
-    LOG_FILE="/var/log/auto_updater_ffjs.log"
+
     # Setup the cronjob for auto-update
 (crontab -l 2>/dev/null; echo "0 * * * * $AUTO_UPDATER_SCRIPT >> $LOG_FILE 2>&1") | crontab -
     cecho "GREEN" "auto-update is setup $AUTO_UPDATER_SCRIPT"
 
 }
-create_auto_update_job
 
 # Restart on changes
-restart_on_changes() {
-    RESTART_SCRIPT="/usr/local/bin/restart_change_ffjs.sh"
+function restart_on_changes() {
+
     sudo tee $RESTART_SCRIPT > /dev/null <<EOL
 #!/bin/bash
 
 echo "Listening..."
 # Define the project directory
-PROJECT_DIR=\$HOME/fleet-flows-js
+PROJECT_DIR=$HOME/fleet-flows-js
 LOG_FILE="$HOME/restart_change_ffjs.log"
 
 # List of files to monitor within the project directory
@@ -420,14 +323,11 @@ EOL
     sudo chmod +rx $RESTART_SCRIPT
     cecho "GREEN" "Restart Script is setup $RESTART_SCRIPT"
 }
-restart_on_changes
-
 
 # Create Fleet Flow JS Listener Service
-create_fleet_flow_js_listener_service() {
-    SERVICE_FILE="/etc/systemd/system/fleet-flows-js-listener.service"
+function create_fleet_flow_js_listener_service() {
 
-    sudo tee $SERVICE_FILE > /dev/null <<EOL
+    sudo cat >/etc/systemd/system/fleet-flows-js-listener.service <<EOL
 [Unit]
 Description=Fleet Flow JS File Change Listener
 After=network.target
@@ -451,4 +351,119 @@ EOL
 
     cecho "GREEN" "Fleet Flows JS Listener Service is setup and started. "
 }
+
+#-------------------------------------------------------------------------------
+
+while [ "$#" -gt 0 ]; do
+  case "$1" in
+    -d|--debug)
+      debug=true
+      shift
+      ;;
+    -nou|--no-update)
+      noupdate=false
+      shift
+      ;;
+    -k|--key)
+      AIRTABLE_API_KEY="$2"
+      shift 2
+      ;;
+    *)
+      echo "Error: Unsupported flag $1" >&2
+      exit 1
+      ;;
+  esac
+done
+
+
+
+debug_echo "DEBUG" "Debugging "
+
+
+if ! check_git_access; then
+	
+	if [ -z "$AIRTABLE_API_KEY" ]; then
+	    cecho "RED" "Error: No Airtable API key provided."
+	    cecho "CYAN" "Usage: $0 -k <AIRTABLE_API_KEY>"
+	    exit 1
+	else
+		
+		debug_echo "DEBUG" "calling check_airtable_api_key"
+		check_airtable_api_key
+		
+		# Check and generate SSH key
+		if [ ! -f "$SSH_KEY_PATH" ]; then
+		  cecho "GREEN" "Generating new SSH key..."
+		  ssh-keygen -t rsa -b 4096 -f $SSH_KEY_PATH -N ""
+			
+			update_ssh_key_in_airtable
+		fi
+		
+	fi 
+fi
+
+
+debug_echo "DEBUG" "updating npm to the latest version"
+
+debug_echo "DEBUG" " installing specific version of node"
+
+cecho "GREEN" "Node.js and npm are updated to the latest versions."
+
+# Install Node.js, npm, and Node-RED
+ensure_installed inotify-tools 
+ensure_installed git
+ensure_installed jq
+ensure_installed nano
+
+
+debug_echo "DEBUG" "checking if n is installed"
+N_CMD=$(which n 2>/dev/null)
+if [ "${N_CMD}" ]; then
+	debug_echo "DEBUG" "installed ${N_CMD}"
+else
+  cecho "YELLOW" "n is not installed. Installing n..."
+  # Install n (Node.js version manager)
+  sudo npm install -g n
+fi
+sudo n install 18 
+
+
+debug_echo "DEBUG" "checking if node-red is installed"
+NRED_CMD=$(which node-red 2>/dev/null)
+if [ "${NRED_CMD}" ]; then
+	debug_echo "DEBUG" "installed ${NRED_CMD}"
+else
+  bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered)  --confirm-install  --node18  
+fi
+
+
+
+pushd $HOME
+	# Function to clone Git repositories
+	debug_echo "DEBUG" "cloning repository"
+
+	# Clone repositories
+	clone_repository "fleet-files" ${BRANCH}
+	clone_repository "fleet-flows-js" ${BRANCH}
+
+	# Run npm install in fleet-flows-js
+	pushd fleet-flows-js
+		npm install
+		create_env_file
+	popd
+popd
+
+create_systemd_service
+restart_on_changes
 create_fleet_flow_js_listener_service
+create_auto_update_job
+
+
+
+debug_echo "DEBUG" "noupdate flasg is: $noupdate"
+if [ "$noupdate" = true ]; then
+  debug_echo "DEBUG" "running update"
+  sudo apt -y update
+  debug_echo "DEBUG" "running upgrade"
+  sudo apt -y upgrade
+fi
